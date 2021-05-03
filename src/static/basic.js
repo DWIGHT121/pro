7dee1abc1068de32d69473d5f5fcdcb759c83410
DWIGHT121 <66079119+DWIGHT121@users.noreply.github.com>
2021-05-03 08:52:29 +0545

    first

---
+++ src/static/basic.js
@@ -0,0 +1,11 @@
+function btnchange(){
+    
+    
+    console.log("CLicked button ")
+}
+
+function mouseover(){
+    document.querySelector("button").style.backgroundcolor = "grey";
+}
+
+// var c = document.querySelectorAll(class = "button")
